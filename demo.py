6069dbb21ade01ac8262812dc72f43168ef81986
Seun Soetan <adewemimo03@gmail.com>
2019-11-22 12:09:25 -0500

    commit list_calc file and update on demo

--- demo.py
+++ demo.py
@@ -14,6 +14,12 @@ from maths.area import (
     area_square
 )
 
+from maths.list_calc import (
+    addition1,
+    substraction1,
+    multiplication1
+)
+
 print ("addition" , addition(3,2))
 print ("substraction", subtraction(3,2))
 print ("square root", square_root(4))
@@ -26,3 +32,14 @@ print('Triangle1', area_triangle1(2, 4))  # 4
 print('Circle2', area_circle2(4))  #50.272
 print('Rectangle', area_rectangle(2, 4)) # 8
 print('Square', area_square(4))  # 16
+
+
+print("addition of numbers" , addition1(2,3,4,5,6)) #20
+print("addition of numbers" , addition1(5)) #5
+print("substraction of numbers" , substraction1(40, 50, 5)) #-15
+print('substraction of numbers' ,  substraction1(8)) #8
+print('multiplication of numbers' , multiplication1(2,2,2,2,2)) #32
+print('multiplication  of numbers' , multiplication1(2)) #2
+
+
+
